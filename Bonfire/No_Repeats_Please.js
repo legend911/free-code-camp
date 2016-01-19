3d37f73dd88cda07b30fd87e42908037637e88f3
Dwight Norris <norrisdb@yahoo.com>
2016-01-19 01:27:38 +0000

    Pre-Draft for No Repeats

---
+++ Bonfire/No_Repeats_Please.js
@@ -0,0 +1,9 @@
+function permAlone(str) {
+    // split the variable "str" into an array
+    
+    // create a multiple loops that test taking the last variable and moving it to position 0 -> (n - 1)
+    // test each of these possibilites for "no repeats" and save it if it is true else move on.
+  return str;
+}
+
+permAlone('aab');
