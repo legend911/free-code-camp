387ae171edab8f6082a15be0f94e8a29267f79ed
Dwight Norris <norrisdb@yahoo.com>
2016-03-07 02:40:29 +0000

    Completed HTTP File Server

--- program.js
+++ program.js
@@ -188,3 +188,15 @@ var server = net.createServer(function (socket) {
 })  
 server.listen(process.argv[2]) 
 */
+
+// HTTP File Server
+/*
+var http = require('http');
+var fs = require("fs");
+
+var server = http.createServer(function (request, response) {  
+    var data = fs.createReadStream(process.argv[3]);
+    data.pipe(response);
+}) 
+server.listen(process.argv[2]);
+*/
