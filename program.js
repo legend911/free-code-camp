8d7188e15d0947e0d070fd001ec5ebc18ef3df08
Dwight Norris <norrisdb@yahoo.com>
2016-03-08 15:13:16 +0000

    Completed HTTP UpperCaserer

--- program.js
+++ program.js
@@ -200,3 +200,16 @@ var server = http.createServer(function (request, response) {
 }) 
 server.listen(process.argv[2]);
 */
+
+// HTTP UpperCaserer
+var http = require('http');
+var fs = require("fs");
+var map = require('through2-map');
+
+var server = http.createServer(function (request, response) {  
+    request.pipe(map(function (chunk) {
+        return chunk.toString().toUpperCase();
+    })).pipe(response);
+    
+}) 
+server.listen(process.argv[2]);
