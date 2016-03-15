899f0a99b1070704e9be772874a1bdb641fe3865
Dwight Norris <norrisdb@yahoo.com>
2016-03-15 01:53:40 +0000

    Completed What's in Query

--- program.js
+++ program.js
@@ -353,3 +353,12 @@ app.listen(process.argv[2]);
 */
 
 // Express (What's in Query)
+var express = require('express');
+var app = express();
+
+app.get('/search', function(req, res) {
+    var id = req.query;
+    res.send(id);
+});
+
+app.listen(process.argv[2]);
