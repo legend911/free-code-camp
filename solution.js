491e099b36e922cbe1169f4f8ee385cf1e7c30a2
Dwight Norris <norrisdb@yahoo.com>
2016-03-20 20:20:37 +0000

    Completed MongoDB Insert and Update

--- solution.js
+++ solution.js
@@ -21,6 +21,7 @@ mongo.connect(url, function(err, db) {
 */
 
 // MongoDB (Find Project)
+/*
 var mongo = require('mongodb').MongoClient
 var url = 'mongodb://localhost:27017/learnyoumongo';
 var newArg = parseInt(process.argv[2]);
@@ -40,6 +41,55 @@ mongo.connect(url, function(err, db) {
   }).toArray(function(err, documents) {
     if (err) throw err;
     console.log(documents);
+    db.close();
+  })
+})
+*/
+
+// MongoDB (Insert)
+/*
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/learnyoumongo';
+var firstName = process.argv[2];
+var lastName = process.argv[3];
+var jsonDoc = {
+  firstName: firstName ,
+  lastName: lastName
+};
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var collection = db.collection('docs');
+  
+  // inserting document
+  collection.insert(jsonDoc, function(err, data) {
+    // handle error
+    if (err) throw err;
+    console.log(JSON.stringify(jsonDoc));
+    // other operations
+    db.close();
+  })
+})
+*/
+
+// MongoDB (Update)
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/' + process.argv[2];
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var collection = db.collection('users');
+  
+  collection.update({
+    username: "tinatime"
+  }, {
+    $set: {
+      age: 40
+    }
+  }, function(err, data) {
+    // handle error
+    if (err) throw err;
+    // other operations
     db.close();
   })
 })
